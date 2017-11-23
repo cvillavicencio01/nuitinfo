ec3dc679efba01504b052c432e7c97141ec8269d
Arnaud-Marie Gallardo <arnaud-marie.gallardo@etu.u-bordeaux.fr>
2017-11-24 00:44:17 +0100

    Add logo

--- app/src/elements/Footer.vue
+++ app/src/elements/Footer.vue
@@ -10,15 +10,35 @@
                 - Sources : <a href="https://github.com/asso-labeli/nuitinfo" target="_blank">GitHub</a>
             </div>
             <div class="credits">
-                En partenariat avec :
-                <div id="labri"><a
-                        href="http://www.u-bordeaux.fr/Colleges-et-departements/College-Sciences-et-technologies/Unite-de-formation-d-informatique"
-                        target="_blank">UF Info</a></div>
-                <div id="cremi"><a href="http://www.emi.u-bordeaux.fr/" target="_blank">CREMI</a></div>
-                <div id="fsdie"><a href="http://www.u-bordeaux.fr/" target="_blank">Université de Bordeaux</a></div>
-                <div id="cgi"><a href="https://www.groupeonepoint.com/emploi-carriere/offres-demplois/" target="_blank">OnePoint</a></div>
-                <div id="iut"><a href="http://www.iut.u-bordeaux.fr/general/" target="_blank">IUT Bordeaux</a></div>
-                <div id="abeii"><a href="http://www.iut.u-bordeaux.fr/general/" target="_blank">Abeii</a></div>
+                <span class="sponsor_text">En partenariat avec :</span>
+                <div id="labri">
+                  <a href="http://www.u-bordeaux.fr/Colleges-et-departements/College-Sciences-et-technologies/Unite-de-formation-d-informatique" target="_blank" class="sponsor_text">
+                    UF Informatique
+                  </a>
+                </div>
+                <div id="cremi">
+                  <a href="http://www.emi.u-bordeaux.fr/" target="_blank">
+                    <img src="http://www.emi.u-bordeaux.fr/images/logo-Cremi-4.png" class="sponsor_img"/>
+                  </a>
+                </div>
+                <div id="fsdie">
+                  <a href="http://www.u-bordeaux.fr/" target="_blank">
+                    <img src="http://www.planetecampus.com/wp-content/uploads/2016/10/UBX.png" class="sponsor_img"/>
+                  </a>
+                </div>
+                <div id="cgi">
+                  <a href="https://www.groupeonepoint.com/emploi-carriere/offres-demplois/" target="_blank">
+                    <img src="https://www.groupeonepoint.com/wp-content/uploads/2016/04/Onepoint_Logo-white.png" class="sponsor_img"/>
+                  </a>
+                </div>
+                <div id="iut">
+                  <a href="http://www.iut.u-bordeaux.fr/general/" target="_blank">
+                    <img src="http://connect-lab.fr/wp-content/uploads/2015/03/logo_info_couleur.png" class="sponsor_img"/>
+                  </a>
+                </div>
+                <div id="abeii">
+                  <a href="http://www.iut.u-bordeaux.fr/general/" target="_blank" class="sponsor_text">Abeii</a>
+                </div>
             </div>
         </div>
     </footer>
